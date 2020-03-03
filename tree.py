dd14416c09c24fe1477435c39874500ce4bb6729
hubte1g <lickteig.joshua@gmail.com>
2020-03-03 08:53:53 -0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,29 @@
+# def say_hello():
+#     print('Hello, World')
+
+# for i in range(5):
+#     say_hello()
+    
+
+    
+input = [(1,2),(2,3),(3,4),(4,5)]
+
+# def new_func():
+#     for i in input:
+#         #print(i)
+#         if i == (1,2):
+#             print(i)
+
+        
+
+# new_func()
+
+def findPath( root, path, k):
+    if root is None:
+        return False
+    
+    
+    else:
+        return True
+
+findPath(0,0,0)
